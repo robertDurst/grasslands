7f67210c32195b65ea27e91d6eee3a5bf2faa2f8
robertDurst <me@robdurst.com>
2025-08-12 12:12:58 -0600

    feat: implement compiler pipeline with lexer, parser, semantic analyzer, IR gen, and code gen stages

--- lib/Lib.hs
+++ lib/Lib.hs
@@ -1,7 +1,47 @@
 module Lib
-  ( foobar,
+  ( lexicalAnalysis,
+    parser,
+    semanticAnalysis,
+    irGeneration,
+    optimization,
+    codeGeneration,
+    linkingAndAssembly,
+    Token,
+    AST,
+    IR,
   )
 where
 
-foobar :: IO ()
-foobar = putStrLn "foobar"
+type Token = String
+
+type AST = String
+
+type IR = String
+
+-- Step 1: Lexical Analysis
+lexicalAnalysis :: String -> [Token]
+lexicalAnalysis _ = []
+
+-- Step 2: Parsing
+parser :: [Token] -> AST
+parser _ = ""
+
+-- Step 3: Semantic Analysis and Type Checking
+semanticAnalysis :: AST -> AST
+semanticAnalysis _ = ""
+
+-- Step 4: IR Generation
+irGeneration :: AST -> IR
+irGeneration _ = ""
+
+-- Step 5: Optimization
+optimization :: IR -> IR
+optimization _ = ""
+
+-- Step 6: Code Generation
+codeGeneration :: IR -> String
+codeGeneration _ = ""
+
+-- Step 7: Linking and Assembly
+linkingAndAssembly :: String -> String
+linkingAndAssembly _ = ""
--- test/Tests.hs
+++ test/Tests.hs
@@ -1,9 +1,34 @@
 module Main (main) where
 
+import Lib qualified
 import Test.Hspec
 
 main :: IO ()
 main = hspec $ do
-  describe "foobar" $ do
-    it "should be foobar" $ do
-      "foobar" `shouldBe` "foobar"
+  describe "Lexical Analysis" $ do
+    it "should return a list of tokens" $ do
+      Lib.lexicalAnalysis "" `shouldBe` []
+
+  describe "Parser" $ do
+    it "should return an AST" $ do
+      Lib.parser [] `shouldBe` ""
+
+  describe "Semantic Analysis" $ do
+    it "should return an AST" $ do
+      Lib.semanticAnalysis "" `shouldBe` ""
+
+  describe "IR Generation" $ do
+    it "should return an IR" $ do
+      Lib.irGeneration "" `shouldBe` ""
+
+  describe "Optimization" $ do
+    it "should return an IR" $ do
+      Lib.optimization "" `shouldBe` ""
+
+  describe "Code Generation" $ do
+    it "should return a String" $ do
+      Lib.codeGeneration "" `shouldBe` ""
+
+  describe "Linking and Assembly" $ do
+    it "should return a String" $ do
+      Lib.linkingAndAssembly "" `shouldBe` ""

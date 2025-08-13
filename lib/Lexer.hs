f8c24f3ff3bc2920ee8580bb3cc2b33e2285b8a5
robertDurst <me@robdurst.com>
2025-08-12 21:45:48 -0600

    refactor: move lexer code into separate Lexer module

---
+++ lib/Lexer.hs
@@ -0,0 +1,36 @@
+module Lexer
+  ( lexicalAnalysis,
+    Token (..),
+  )
+where
+
+import Data.Char (isAlpha, isDigit, isSpace)
+
+data Token
+  = PercentageToken Double
+  | WordToken String
+  | PeriodToken
+  deriving (Show, Eq)
+
+-- Step 1: Lexical Analysis
+lexicalAnalysis :: String -> [Token]
+lexicalAnalysis "" = []
+lexicalAnalysis input@(c : cs)
+  | isAlpha c =
+      let (word, rest) = span isAlpha input
+       in WordToken word : lexicalAnalysis rest
+  | isDigit c =
+      let (intPart, afterInt) = span isDigit input
+          (number, rest) = case afterInt of
+            ('.' : cs') ->
+              let (fracPart, afterFrac) = span isDigit cs'
+               in (intPart ++ "." ++ fracPart, afterFrac)
+            _ -> (intPart, afterInt)
+       in case rest of
+            ('%' : rest') -> PercentageToken (read number) : lexicalAnalysis rest'
+            _ -> errorWithoutStackTrace ("Expected '%' after number: " ++ number)
+  | isSpace c = lexicalAnalysis cs
+  | otherwise = case c of
+      '.' -> PeriodToken : lexicalAnalysis cs
+      '%' -> errorWithoutStackTrace "Expected '%' after number. '%' found alone"
+      _ -> errorWithoutStackTrace ("Unexpected character: " ++ [c])
--- lib/Lib.hs
+++ lib/Lib.hs
@@ -12,40 +12,11 @@ module Lib
   )
 where
 
-import Data.Char (isAlpha, isDigit, isSpace)
-
-data Token
-  = PercentageToken Double
-  | WordToken String
-  | PeriodToken
-  deriving (Show, Eq)
+import Lexer (Token (..), lexicalAnalysis)
 
 type AST = String
 
 type IR = String
-
--- Step 1: Lexical Analysis
-lexicalAnalysis :: String -> [Token]
-lexicalAnalysis "" = []
-lexicalAnalysis input@(c : cs)
-  | isAlpha c =
-      let (word, rest) = span isAlpha input
-       in WordToken word : lexicalAnalysis rest
-  | isDigit c =
-      let (intPart, afterInt) = span isDigit input
-          (number, rest) = case afterInt of
-            ('.' : cs') ->
-              let (fracPart, afterFrac) = span isDigit cs'
-               in (intPart ++ "." ++ fracPart, afterFrac)
-            _ -> (intPart, afterInt)
-       in case rest of
-            ('%' : rest') -> PercentageToken (read number) : lexicalAnalysis rest'
-            _ -> errorWithoutStackTrace ("Expected '%' after number: " ++ number)
-  | isSpace c = lexicalAnalysis cs
-  | otherwise = case c of
-      '.' -> PeriodToken : lexicalAnalysis cs
-      '%' -> errorWithoutStackTrace "Expected '%' after number. '%' found alone"
-      _ -> errorWithoutStackTrace ("Unexpected character: " ++ [c])
 
 -- Step 2: Parsing
 parser :: [Token] -> AST

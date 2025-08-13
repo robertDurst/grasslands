02a6a2887fd79d7cdad204de80dade960440dbb2
robertDurst <me@robdurst.com>
2025-08-12 22:23:13 -0600

    refactor: remove qualified imports and use direct imports in test file

--- test/Tests.hs
+++ test/Tests.hs
@@ -1,7 +1,7 @@
 module Tests (main) where
 
 import Control.Exception (evaluate)
-import Main qualified
+import Main (Component (..), System (..), Token (..), lexicalAnalysis, makeComponent, parser, semanticAnalysis)
 import Test.Hspec
 
 main :: IO ()
@@ -9,46 +9,46 @@ main = hspec $ do
   describe "Lexical Analysis" $ do
     describe "Empty String" $ do
       it "should return an empty list for an empty string" $ do
-        Main.lexicalAnalysis "" `shouldBe` []
+        lexicalAnalysis "" `shouldBe` []
     describe "Words" $ do
       it "should tokenize words" $ do
-        Main.lexicalAnalysis "hello world" `shouldBe` [Main.WordToken "hello", Main.WordToken "world"]
+        lexicalAnalysis "hello world" `shouldBe` [WordToken "hello", WordToken "world"]
     describe "Periods" $ do
       it "should tokenize period" $ do
-        Main.lexicalAnalysis "." `shouldBe` [Main.PeriodToken]
+        lexicalAnalysis "." `shouldBe` [PeriodToken]
     describe "Percentages" $ do
       it "should throw error for empty percentages" $ do
-        evaluate (Main.lexicalAnalysis "%") `shouldThrow` anyErrorCall
+        evaluate (lexicalAnalysis "%") `shouldThrow` anyErrorCall
       it "should tokenize int percentage" $ do
-        Main.lexicalAnalysis "1%" `shouldBe` [Main.PercentageToken 1]
+        lexicalAnalysis "1%" `shouldBe` [PercentageToken 1]
       it "should tokenize float percentage" $ do
-        Main.lexicalAnalysis "1.1%" `shouldBe` [Main.PercentageToken 1.1]
+        lexicalAnalysis "1.1%" `shouldBe` [PercentageToken 1.1]
 
   describe "Parser" $ do
     describe "Empty List" $ do
       it "should return a System" $ do
-        Main.parser [] `shouldBe` Main.System {Main.systemComponents = []}
+        parser [] `shouldBe` System {systemComponents = []}
     describe "Simple System" $ do
       it "should return a System" $
         do
-          Main.parser $ Main.lexicalAnalysis "AwesomeReact expects an uptime of 99.9% and depends on StableBackend and ReliableAuthentication."
-          `shouldBe` Main.System
-            { Main.systemComponents =
-                [ Main.makeComponent "AwesomeReact" 99.9 ["StableBackend", "ReliableAuthentication"]
+          parser $ lexicalAnalysis "AwesomeReact expects an uptime of 99.9% and depends on StableBackend and ReliableAuthentication."
+          `shouldBe` System
+            { systemComponents =
+                [ makeComponent "AwesomeReact" 99.9 ["StableBackend", "ReliableAuthentication"]
                 ]
             }
 
   describe "Semantic Analysis" $ do
     describe "Empty System" $ do
       it "should return True" $ do
-        Main.semanticAnalysis Main.System {Main.systemComponents = []} `shouldBe` True
+        semanticAnalysis System {systemComponents = []} `shouldBe` True
     describe "Invalid Simple System" $ do
       it "should return False if mentions non-existent dependencies" $ do
-        Main.semanticAnalysis
-          ( Main.System
-              { Main.systemComponents =
-                  [ Main.makeComponent "AwesomeReact" 99.9 ["StableBackend", "ReliableAuthentication"],
-                    Main.makeComponent "StableBackend" 99.95 []
+        semanticAnalysis
+          ( System
+              { systemComponents =
+                  [ makeComponent "AwesomeReact" 99.9 ["StableBackend", "ReliableAuthentication"],
+                    makeComponent "StableBackend" 99.95 []
                   ]
               }
           )
@@ -56,12 +56,12 @@ main = hspec $ do
 
     describe "Valid Simple System" $ do
       it "should return True if all dependencies exist" $ do
-        Main.semanticAnalysis
-          ( Main.System
-              { Main.systemComponents =
-                  [ Main.makeComponent "AwesomeReact" 99.9 ["StableBackend", "ReliableAuthentication"],
-                    Main.makeComponent "StableBackend" 99.95 [],
-                    Main.makeComponent "ReliableAuthentication" 99.99 []
+        semanticAnalysis
+          ( System
+              { systemComponents =
+                  [ makeComponent "AwesomeReact" 99.9 ["StableBackend", "ReliableAuthentication"],
+                    makeComponent "StableBackend" 99.95 [],
+                    makeComponent "ReliableAuthentication" 99.99 []
                   ]
               }
           )
